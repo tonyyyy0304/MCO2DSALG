bd04b4a0e07254c5593d99910ce2d2e298acb6a4
PogiPhantom <64260427+PogiPhantom@users.noreply.github.com>
2024-07-29 00:40:05 +0800

    DFS with comments
    Added inline comments

--- DFS.c
+++ DFS.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-
+// creates a stack
 void createStack(struct Stack *stack, struct Graph *graph){
 
     stack->top = -1;
@@ -14,15 +14,19 @@ void createStack(struct Stack *stack, struct Graph *graph){
      }        
 }
 
+// checks if stack is empty
 int is_empty(struct Stack *stack){
     return stack->top == -1;
 }
 
+// pushes onto the stack
 void push(struct Stack *stack, int vertex){
     if (stack->top < (stack->max - 1)){
         stack->Stack[++stack->top] = vertex;
     }
 }
+
+// pops from stack
 int pop(struct Stack* stack){
     if(is_empty(stack)){
         return -1;
@@ -31,14 +35,15 @@ int pop(struct Stack* stack){
 }
 
 void depthFirstSearch(struct Stack *stack, struct Graph *graph, char *startVertex, FILE* fp) {
-    int visited[100] = {0};  // Adjust size to maxVertex if possible
-    int current;
-    int vertexNum;
+    int visited[100] = {0};  // Array containing visited vertices
+    int current; 
+    int vertexNum; 
 
-    vertexNum = getNameIndex(startVertex, graph);
+    vertexNum = getNameIndex(startVertex, graph); 
     createStack(stack, graph);
     push(stack, vertexNum);
 
+    //Dfs algorithm
     while (!is_empty(stack)) {
         current = pop(stack);
         if (!visited[current]) {
